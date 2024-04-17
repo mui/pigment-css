9b6035ade3de71d489bb0e8738039d2e94fbe472
Brijesh Bittu <brijeshb42@gmail.com>
2024-04-17 12:06:33 +0530

    Remove processor for default import

--- packages/pigment-css-react/tests/Box/fixtures/box.output.js
+++ packages/pigment-css-react/tests/Box/fixtures/box.output.js
@@ -3,7 +3,7 @@ import { ForwardSx as _ForwardSx } from '@pigment-css/react/private-runtime';
 import Box from '@pigment-css/react/Box';
 export function App() {
   return (
-    <_ForwardSx sxComponent={Box} as="ul" sx={'_1yemdgw'}>
+    <_ForwardSx sxComponent={Box} as="ul" sx={'_c1d15y'}>
       Hello Box
     </_ForwardSx>
   );
