45526647708b155b75ce83c77252824c55f97f05
Brijesh Bittu <brijeshb42@gmail.com>
2024-04-19 16:11:57 +0530

    [react][core] Add support for sx replacement in transformed jsx calls (#22)

---
+++ packages/pigment-css-react/tests/Box/fixtures/box-jsx.output.js
@@ -0,0 +1,12 @@
+import { ForwardSx as _ForwardSx } from '@pigment-css/react/private-runtime';
+import Box from '@pigment-css/react/Box';
+import { jsx as _jsx } from 'react/jsx-runtime';
+export function App(props) {
+  return /*#__PURE__*/ _jsx(_ForwardSx, {
+    sxComponent: Box,
+    as: 'ul',
+    'aria-label': props.label,
+    sx: 'sd5jss7',
+    children: 'Hello Box',
+  });
+}
--- packages/pigment-css-react/tests/Box/fixtures/box.output.js
+++ packages/pigment-css-react/tests/Box/fixtures/box.output.js
@@ -1,9 +1,19 @@
 import { sx as _sx2 } from '@pigment-css/react';
 import { ForwardSx as _ForwardSx } from '@pigment-css/react/private-runtime';
 import Box from '@pigment-css/react/Box';
-export function App() {
+export function App(props) {
   return (
-    <_ForwardSx sxComponent={Box} as="ul" sx={'_c1d15y'}>
+    <_ForwardSx
+      sxComponent={Box}
+      as="ul"
+      aria-label={props.label}
+      sx={{
+        className: '_c1d15y',
+        vars: {
+          '_c1d15y-0': [props.color, false],
+        },
+      }}
+    >
       Hello Box
     </_ForwardSx>
   );
