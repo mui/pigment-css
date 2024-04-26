2f6b20973060133cf5833b8509fca0ef8796a5bd
siriwatknp <siriwatkunaporn@gmail.com>
2024-04-26 10:51:04 +0700

    update test

--- packages/pigment-css-react/tests/Box/fixtures/box-jsx.output.js
+++ packages/pigment-css-react/tests/Box/fixtures/box-jsx.output.js
@@ -1,4 +1,4 @@
-import { sx as _sx } from '@pigment-css/react';
+import { sx as _sx, sx as _sx2 } from '@pigment-css/react';
 import Box from '@pigment-css/react/Box';
 import { jsx as _jsx } from 'react/jsx-runtime';
 export function App(props) {
@@ -8,4 +8,21 @@ export function App(props) {
     children: 'Hello Box',
     ..._sx('sd5jss7', {}),
   });
+}
+function App2(props) {
+  return /*#__PURE__*/ _jsxDEV(
+    Box,
+    {
+      children: 'Test',
+      ..._sx2('sk625fs', {}),
+    },
+    void 0,
+    false,
+    {
+      fileName: '',
+      lineNumber: 11,
+      columnNumber: 11,
+    },
+    this,
+  );
 }
