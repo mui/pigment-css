d023730fbc21a1c112c2f2b9b6bf441302f0afc2
siriwatknp <siriwatkunaporn@gmail.com>
2024-05-06 10:42:13 +0700

    support array at runtime

--- packages/pigment-css-react/src/sx.js
+++ packages/pigment-css-react/src/sx.js
@@ -1,7 +1,23 @@
 export default function sx(transformedSx, { className, style }) {
-  const sxClass = typeof transformedSx === 'string' ? transformedSx : transformedSx?.className;
-  const sxVars =
-    transformedSx && typeof transformedSx !== 'string' ? transformedSx.vars : undefined;
+  let sxClass = '';
+  let sxVars = {};
+
+  function iterateSx(element) {
+    sxClass += ` ${typeof element === 'string' ? element : element.className}`;
+    sxVars = {
+      ...sxVars,
+      ...(transformedSx && typeof transformedSx !== 'string' ? transformedSx.vars : undefined),
+    };
+  }
+
+  if (Array.isArray(transformedSx)) {
+    transformedSx.forEach((element) => {
+      iterateSx(element);
+    });
+  } else {
+    iterateSx(transformedSx);
+  }
+
   const varStyles = {};
 
   if (sxVars) {
