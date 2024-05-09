3257fccd75a3ade773ba03d99845b679f023a655
Brijesh Bittu <brijeshb42@gmail.com>
2024-05-09 18:49:34 +0530

    [core] Cleanup (#63)

--- test/karma.tests.js
+++
@@ -1,9 +0,0 @@
-import '@mui/internal-test-utils/init';
-import '@mui/internal-test-utils/setupKarma';
-
-const pigmentCssContext = require.context(
-  '../packages/pigment-css-react/tests/',
-  true,
-  /\.test\.(js|ts|tsx)$/,
-);
-pigmentCssContext.keys().forEach(pigmentCssContext);
