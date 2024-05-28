f33d4f9abe0bef298f8b5c2bc8eb5129d31faa08
Brijesh Bittu <brijeshb42@gmail.com>
2024-05-28 12:30:26 +0530

    [fix] Make theme optional in Pigment CSS config (#108)

--- packages/pigment-css-react/src/utils/generateCss.ts
+++ packages/pigment-css-react/src/utils/generateCss.ts
@@ -1,13 +1,16 @@
 import { serializeStyles } from '@emotion/serialize';
 import { Theme } from './extendTheme';
 
-export function generateTokenCss(theme: Theme) {
+export function generateTokenCss(theme?: Theme) {
+  if (!theme) {
+    return '';
+  }
   // use emotion to serialize the object to css string
   const { styles } = serializeStyles(theme.generateStyleSheets?.() || []);
   return styles;
 }
 
-export function generateThemeTokens(theme: Theme) {
+export function generateThemeTokens(theme?: Theme) {
   if (!theme || typeof theme !== 'object') {
     return {};
   }
