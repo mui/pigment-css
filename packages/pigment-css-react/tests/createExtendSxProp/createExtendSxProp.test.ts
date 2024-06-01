e8131b42c02c34729ce9cc1bfdcbbca8838002ea
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-01 12:11:53 -0400

    fix tests

--- packages/pigment-css-react/tests/createExtendSxProp/createExtendSxProp.test.ts
+++ packages/pigment-css-react/tests/createExtendSxProp/createExtendSxProp.test.ts
@@ -1,6 +1,5 @@
 import path from 'node:path';
 import { expect } from 'chai';
-import { createExtendSxProp } from '@pigment-css/react';
 import { runTransformation } from '../testUtils';
 
 describe('Pigment CSS - createExtendSxProp', () => {
@@ -11,11 +10,5 @@ describe('Pigment CSS - createExtendSxProp', () => {
 
     expect(output.js).to.equal(fixture.js);
     expect(output.css).to.equal(fixture.css);
-  });
-
-  it('return the new copy of input', () => {
-    const original = { color: 'red' };
-    expect(createExtendSxProp()(original)).to.not.equal(original);
-    expect(createExtendSxProp()(original)).to.deep.equal({ color: 'red' });
   });
 });
