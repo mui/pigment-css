be951bff64a83ade7473ed8b5740f5a30d3c4531
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-01 13:21:30 -0400

    add comment

--- packages/pigment-css-react/src/utils/processCssObject.ts
+++ packages/pigment-css-react/src/utils/processCssObject.ts
@@ -7,9 +7,10 @@ export function processCssObject(
   themeArgs?: PluginCustomOptions['themeArgs'],
   skipSx = true,
 ) {
-  const processedObj = (
-    skipSx ? cssObj : themeArgs?.theme?.unstable_sx?.(cssObj) || cssObj
-  ) as CSSObject;
+  const processedObj =
+    // `unstable_sx` is currently an internal API for integrating Material UI with Pigment CSS only.
+    // so for Pigment CSS users, the shorthand `sx` prop is not supported yet.
+    (skipSx ? cssObj : themeArgs?.theme?.unstable_sx?.(cssObj) || cssObj) as CSSObject;
   const className = css(processedObj);
   return cache.registered[className];
 }
