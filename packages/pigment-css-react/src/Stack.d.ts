1afa0d91ce6a6a8cbd9bee6f8a2a0b546f032c61
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-17 08:31:01 +0700

    remove outdated types

--- packages/pigment-css-react/src/Stack.d.ts
+++ packages/pigment-css-react/src/Stack.d.ts
@@ -6,11 +6,8 @@ import { PolymorphicComponent } from './Box';
 type CssProperty<T> = T | Array<T> | Partial<Record<Breakpoint, T>>;
 
 type StackBaseProps = {
-  display?: CssProperty<'flex' | 'inline-flex'>;
   spacing?: CssProperty<number | string>;
   direction?: CssProperty<CSS.StandardLonghandProperties['flexDirection']>;
-  justifyContent?: CssProperty<CSS.StandardLonghandProperties['justifyContent']>;
-  alignItems?: CssProperty<CSS.StandardLonghandProperties['alignItems']>;
   divider?: React.ReactNode;
   className?: string;
 };
