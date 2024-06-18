195ec31a5227931508b39aff3f9f6bb13ba4ddef
Diego Andai <diego@mui.com>
2024-06-18 11:26:26 -0400

    prettier

--- packages/pigment-css-react/src/Grid.jsx
+++ packages/pigment-css-react/src/Grid.jsx
@@ -22,80 +22,81 @@ export const gridAtomics = generateAtomics(({ theme }) => {
       conditions[breakpoint] = `@media (min-width: ${value}${theme.breakpoints.unit ?? 'px'})`;
     });
   return {
-  conditions,
-  defaultCondition: theme.breakpoints?.keys?.[0],
-  properties: {
-    flexDirection: ['column', 'column-reverse', 'row', 'row-reverse'],
-    '--Grid-parent-column-count': ['--Grid-parent-column-count'],
-    '--Grid-parent-column-spacing': ['--Grid-parent-column-spacing'],
-    '--Grid-parent-row-spacing': ['--Grid-parent-row-spacing'],
-    '--Grid-self-column-span': ['--Grid-self-column-span'],
-    '--Grid-self-width': ['--Grid-self-width'],
-    '--Grid-self-max-width': ['--Grid-self-max-width'],
-    '--Grid-self-flex': ['--Grid-self-flex'],
-    '--Grid-self-column-spacing': ['--Grid-self-column-spacing'],
-    '--Grid-self-row-spacing': ['--Grid-self-row-spacing'],
-    '--Grid-self-offset': ['--Grid-self-offset'],
-    '--Grid-self-margin-left': ['--Grid-self-margin-left'],
-  },
-  unitless: ['--Grid-parent-column-count', '--Grid-self-column-span', '--Grid-self-offset'],
-  multipliers: {
-    '--Grid-parent-column-spacing': Array.isArray(theme.vars?.spacing)
-      ? theme.vars.spacing[0]
-      : theme.vars?.spacing,
-    '--Grid-parent-row-spacing': Array.isArray(theme.vars?.spacing)
-      ? theme.vars.spacing[0]
-      : theme.vars?.spacing,
-    '--Grid-self-column-spacing': Array.isArray(theme.vars?.spacing)
-      ? theme.vars.spacing[0]
-      : theme.vars?.spacing,
-    '--Grid-self-row-spacing': Array.isArray(theme.vars?.spacing)
-      ? theme.vars.spacing[0]
-      : theme.vars?.spacing,
-  },
-  inlineGetters: {
-    '--Grid-self-width': (value) => {
-      if (value === 'grow') {
+    conditions,
+    defaultCondition: theme.breakpoints?.keys?.[0],
+    properties: {
+      flexDirection: ['column', 'column-reverse', 'row', 'row-reverse'],
+      '--Grid-parent-column-count': ['--Grid-parent-column-count'],
+      '--Grid-parent-column-spacing': ['--Grid-parent-column-spacing'],
+      '--Grid-parent-row-spacing': ['--Grid-parent-row-spacing'],
+      '--Grid-self-column-span': ['--Grid-self-column-span'],
+      '--Grid-self-width': ['--Grid-self-width'],
+      '--Grid-self-max-width': ['--Grid-self-max-width'],
+      '--Grid-self-flex': ['--Grid-self-flex'],
+      '--Grid-self-column-spacing': ['--Grid-self-column-spacing'],
+      '--Grid-self-row-spacing': ['--Grid-self-row-spacing'],
+      '--Grid-self-offset': ['--Grid-self-offset'],
+      '--Grid-self-margin-left': ['--Grid-self-margin-left'],
+    },
+    unitless: ['--Grid-parent-column-count', '--Grid-self-column-span', '--Grid-self-offset'],
+    multipliers: {
+      '--Grid-parent-column-spacing': Array.isArray(theme.vars?.spacing)
+        ? theme.vars.spacing[0]
+        : theme.vars?.spacing,
+      '--Grid-parent-row-spacing': Array.isArray(theme.vars?.spacing)
+        ? theme.vars.spacing[0]
+        : theme.vars?.spacing,
+      '--Grid-self-column-spacing': Array.isArray(theme.vars?.spacing)
+        ? theme.vars.spacing[0]
+        : theme.vars?.spacing,
+      '--Grid-self-row-spacing': Array.isArray(theme.vars?.spacing)
+        ? theme.vars.spacing[0]
+        : theme.vars?.spacing,
+    },
+    inlineGetters: {
+      '--Grid-self-width': (value) => {
+        if (value === 'grow') {
+          return 'unset';
+        }
+
+        if (value === 'auto') {
+          return 'auto';
+        }
+
+        return 'var(--Grid-fixed-width)';
+      },
+      '--Grid-self-max-width': (value) => {
+        if (value === 'grow') {
+          return '100%';
+        }
+
+        if (value === 'auto') {
+          return 'none';
+        }
+
         return 'unset';
-      }
+      },
+      '--Grid-self-flex': (value) => {
+        if (value === 'grow') {
+          return '1 1 0';
+        }
 
-      if (value === 'auto') {
-        return 'auto';
-      }
+        if (value === 'auto') {
+          return '0 0 auto';
+        }
 
-      return 'var(--Grid-fixed-width)';
+        return '0 1 auto';
+      },
+      '--Grid-self-margin-left': (value) => {
+        if (value === 'auto') {
+          return 'auto';
+        }
+
+        return 'var(--Grid-fixed-offset)';
+      },
     },
-    '--Grid-self-max-width': (value) => {
-      if (value === 'grow') {
-        return '100%';
-      }
-
-      if (value === 'auto') {
-        return 'none';
-      }
-
-      return 'unset';
-    },
-    '--Grid-self-flex': (value) => {
-      if (value === 'grow') {
-        return '1 1 0';
-      }
-
-      if (value === 'auto') {
-        return '0 0 auto';
-      }
-
-      return '0 1 auto';
-    },
-    '--Grid-self-margin-left': (value) => {
-      if (value === 'auto') {
-        return 'auto';
-      }
-
-      return 'var(--Grid-fixed-offset)';
-    },
-  },
-}});
+  };
+});
 
 const GridComponent = styled('div')({
   '--Grid-fixed-width':
