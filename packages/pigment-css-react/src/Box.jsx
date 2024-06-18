de560821b244f6d1e6bc471618b360ef5c6f4fec
Brijesh Bittu <brijeshb42@gmail.com>
2024-06-18 20:47:32 +0530

    [react] Implement Container component (#143)

--- packages/pigment-css-react/src/Box.jsx
+++ packages/pigment-css-react/src/Box.jsx
@@ -52,30 +52,32 @@ const Box = React.forwardRef(
   },
 );
 
-Box.propTypes /* remove-proptypes */ = {
-  // ┌────────────────────────────── Warning ──────────────────────────────┐
-  // │ These PropTypes are generated from the TypeScript type definitions. │
-  // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
-  // └─────────────────────────────────────────────────────────────────────┘
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   * Replacement for the emotion's `as` prop.
-   */
-  as: PropTypes.elementType,
-  /**
-   * The content of the component.
-   */
-  children: PropTypes.node,
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component: PropTypes.elementType,
-  /**
-   * The style extension prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-};
+process.env.NODE_ENV !== 'production'
+  ? (Box.propTypes /* remove-proptypes */ = {
+      // ┌────────────────────────────── Warning ──────────────────────────────┐
+      // │ These PropTypes are generated from the TypeScript type definitions. │
+      // │    To update them, edit the d.ts file and run `pnpm proptypes`.     │
+      // └─────────────────────────────────────────────────────────────────────┘
+      /**
+       * The component used for the root node.
+       * Either a string to use a HTML element or a component.
+       * Replacement for the emotion's `as` prop.
+       */
+      as: PropTypes.elementType,
+      /**
+       * The content of the component.
+       */
+      children: PropTypes.node,
+      /**
+       * The component used for the root node.
+       * Either a string to use a HTML element or a component.
+       */
+      component: PropTypes.elementType,
+      /**
+       * The style extension prop that allows defining system overrides as well as additional CSS styles.
+       */
+      sx: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    })
+  : void 0;
 
 export default Box;
