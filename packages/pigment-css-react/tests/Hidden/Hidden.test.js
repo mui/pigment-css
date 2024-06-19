15df69acc33e6027ff69478d2429e87a8eb7ef7a
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-19 09:41:39 +0700

    fix test

--- packages/pigment-css-react/tests/Hidden/Hidden.test.js
+++ packages/pigment-css-react/tests/Hidden/Hidden.test.js
@@ -1,31 +1,29 @@
 import * as React from 'react';
 import path from 'node:path';
 import { createRenderer } from '@mui/internal-test-utils';
+import { createBreakpoints } from '@mui/system';
 import { runTransformation, expect } from '../testUtils';
-import createBreakpoints from './createBreakpoints';
-import HiddenOutput from './Hidden.output';
 
 describe('Pigment CSS - Hidden', () => {
   const { render } = createRenderer();
 
   it('should transform and render sx prop', async () => {
     const { output, fixture } = await runTransformation(
-      path.join(process.cwd(), 'packages/pigment-css-react/src/Hidden.jsx'),
+      path.join(__dirname, '../../src/Hidden.jsx'),
       {
         themeArgs: {
           theme: {
             breakpoints: createBreakpoints({}),
           },
         },
-        outputJsPath: path.join(
-          process.cwd(),
-          'packages/pigment-css-react/tests/Hidden/Hidden.output.js',
-        ),
+        outputDir: path.join(__dirname, 'fixtures'),
       },
     );
 
     expect(output.js).to.equal(fixture.js);
     expect(output.css).to.equal(fixture.css);
+
+    const HiddenOutput = (await import('./fixtures/Hidden.output.js')).default;
 
     const { container } = render(<HiddenOutput smDown lgUp only={['xs', 'xl']} />);
     const classNames = new Set([...container.firstChild.className.split(' ')]);
