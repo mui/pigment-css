76788296b4dacaf46c95174125cdbe40f152b571
Siriwat K <siriwatkunaporn@gmail.com>
2024-06-19 21:27:20 +0700

    [react] Export `globalCss` from index (#148)

---
+++ packages/pigment-css-react/src/index.spec.ts
@@ -0,0 +1,66 @@
+import { css, globalCss, keyframes, styled, sx } from '@pigment-css/react';
+
+declare module '@pigment-css/react/theme' {
+  interface ThemeArgs {
+    theme: {
+      palette: {
+        primary: {
+          main: string;
+        };
+      };
+    };
+  }
+}
+
+css``;
+css({
+  color: 'red',
+});
+
+globalCss`
+  html {
+    font-size: 16px;
+  }
+`;
+globalCss(({ theme }) => ({
+  body: {
+    color: theme.palette.primary.main,
+  },
+}));
+
+keyframes``;
+keyframes(() => ({
+  from: {
+    opacity: 0,
+  },
+  to: {
+    opacity: 1,
+  },
+}));
+
+styled.div``;
+styled.div(({ theme }) => ({
+  color: theme.palette.primary.main,
+}));
+
+sx({ color: 'red' });
+sx(({ theme }) => ({
+  color: theme.palette.primary.main,
+}));
+sx([
+  { color: 'red' },
+  ({ theme }) => ({
+    color: theme.palette.primary.main,
+  }),
+]);
+const foo = true;
+sx([
+  true && { color: 'red' },
+  foo
+    ? ({ theme }) => ({
+        color: theme.palette.primary.main,
+      })
+    : {
+        color: 'blue',
+      },
+]);
