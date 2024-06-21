8ac0cf97b36ef471a1a720d36ab73789e6cc84e6
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-21 11:51:23 +0700

    fix keyframes

--- packages/pigment-css-react/tests/keyframes/fixtures/keyframes-theme.output.js
+++ packages/pigment-css-react/tests/keyframes/fixtures/keyframes-theme.output.js
@@ -1,2 +1,18 @@
+const green = 'green';
 const gradientKeyframe = 'gpsum18';
 const gradientKeyframe2 = 'g1t1dgxp';
+
+// simulate CssBaseline transpiled by Next.js
+export const styles = (theme) => ({
+  '0%': {
+    background: theme.palette.primary.main,
+  },
+  '50%': {
+    background: green,
+  },
+  '100%': {
+    background: theme.palette.secondary.main,
+  },
+});
+const gradientKeyframe3 = 'g9lh2y5';
+var _c;
