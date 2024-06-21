f59496d4f2d39c6951fcc2c317509334d6b5c5b2
siriwatknp <siriwatkunaporn@gmail.com>
2024-06-21 11:47:02 +0700

    Fix global CSS transpilation case

--- packages/pigment-css-react/tests/globalCss/fixtures/globalCss-theme.output.js
+++ packages/pigment-css-react/tests/globalCss/fixtures/globalCss-theme.output.js
@@ -3,3 +3,12 @@ let inputGlobalStyles = null;
 if (typeof inputGlobalStyles === 'function') {
   inputGlobalStyles = inputGlobalStyles();
 }
+
+// simulate CssBaseline transpiled by Next.js
+export const styles = (theme) => ({
+  html: {
+    color: theme.palette.primary.main,
+  },
+});
+const GlobalStyles = null;
+var _c;
