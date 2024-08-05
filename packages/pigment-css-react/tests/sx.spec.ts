b54c2128bf7ba99cbfdff6d80270045d1a8371b6
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-05 20:21:38 +0700

    add spec and fix sx

---
+++ packages/pigment-css-react/tests/sx.spec.ts
@@ -0,0 +1,17 @@
+import { SxProp } from '../src/sx';
+
+const sx1: SxProp = { color: 'red' };
+
+const sx2: SxProp = (theme) => ({
+  color: theme.palette.primary.main,
+});
+
+const sx3: SxProp = [{ color: 'red' }, { backgroundColor: 'blue', color: 'white' }];
+
+const test = true;
+const sx4: SxProp = [
+  test ? { color: 'red' } : { backgroundColor: 'blue', color: 'white' },
+  (theme) => ({
+    border: `1px solid ${theme.palette.primary.main}`,
+  }),
+];
