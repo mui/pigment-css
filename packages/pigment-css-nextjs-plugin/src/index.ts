mui/pigment-css

commit e1c587ddf873f79c6198e38a7dbbfa57c6168034

[nextjs] Move the import startsWith next check at end (#228)
Co-authored-by: Brijesh Bittu <brijesh42@gmail.com>

packages/pigment-css-nextjs-plugin/src/index.ts
@@ -61,13 +61,7 @@ export function withPigment(nextConfig: NextConfig, pigmentConfig?: PigmentOptio
           if (what.startsWith('__barrel_optimize__')) {
             return require.resolve('../next-font');
           }
-          // Need to point to the react from node_modules during eval time.
-          // Otherwise, next makes it point to its own version of react that
-          // has a lot of RSC specific logic which is not actually needed.
-          if (what.startsWith('@babel') || what.startsWith('react') || what.startsWith('next')) {
-            return require.resolve(what);
-          }
-          if (what === 'next/image') {
+          if (what === 'next/image' || what === 'next/link') {
             return require.resolve('../next-image');
           }
           if (what.startsWith('next/font')) {
@@ -75,6 +69,17 @@ export function withPigment(nextConfig: NextConfig, pigmentConfig?: PigmentOptio
           }
           if (what.startsWith('@emotion/styled') || what.startsWith('styled-components')) {
             return require.resolve('../third-party-styled');
+          }
+          // Need to point to the react from node_modules during eval time.
+          // Otherwise, next makes it point to its own version of react that
+          // has a lot of RSC specific logic which is not actually needed.
+          if (
+            what === 'react' ||
+            what.startsWith('react-dom/') ||
+            what.startsWith('@babel/') ||
+            what.startsWith('next/')
+          ) {
+            return require.resolve(what);
           }
           if (asyncResolve) {
             return asyncResolve(what, importer, stack);

commit a8a814705941470e26276a99e985bd5820601a81

[nextjs] Fix react import check bug introduced in #228

packages/pigment-css-nextjs-plugin/src/index.ts
@@ -75,6 +75,7 @@ export function withPigment(nextConfig: NextConfig, pigmentConfig?: PigmentOptio
           // has a lot of RSC specific logic which is not actually needed.
           if (
             what === 'react' ||
+            what.startsWith('react/') ||
             what.startsWith('react-dom/') ||
             what.startsWith('@babel/') ||
             what.startsWith('next/')