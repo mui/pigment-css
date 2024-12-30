8f013a17dbfcccb2795007cfdec70ecd57d71b45
Brijesh Bittu <brijesh42@gmail.com>
2024-12-30 20:30:55 +0530

    fix(theme): Allow listing values with number keys

--- packages/pigment-css-theme/tests/theme.spec.ts
+++ packages/pigment-css-theme/tests/theme.spec.ts
@@ -1,14 +1,33 @@
 import { t } from '../src';
 
+const theme = {
+  gray: {
+    surface: {
+      1: 'hsl(0 0% 99%)',
+      100: 'hsl(0 0% 99%)',
+      '2': 'hsl(0 0% 97.5%)',
+    },
+  },
+  br: {
+    circle: '50%',
+    pill: '9999px',
+  },
+};
+
+type UserTheme = typeof theme;
+
 declare module '../src' {
-  interface Theme {
-    palette: {
-      main: string;
-    };
-  }
+  interface Theme extends UserTheme {}
 }
 
-t('$palette.main');
+t('$gray.surface.1');
+t('$gray.surface.100');
+t('$gray.surface.2');
+// @ts-expect-error 3 does not exist
+t('$gray.surface.3');
+t('$br.circle');
+// @ts-expect-error circle1 does not exist
+t('$br.circle1');
 
 // @ts-expect-error
 t('$palette.secondary');
