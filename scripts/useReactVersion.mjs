2fe13842662c8132c8108b1ed74fcdb1cda4f445
Haoqun Jiang <haoqunjiang@gmail.com>
2025-04-13 20:27:07 +0800

    ci: fix unit test on CircleCI

--- scripts/useReactVersion.mjs
+++ scripts/useReactVersion.mjs
@@ -75,6 +75,7 @@ async function main(version) {
         packageVersion = tagMapping.replace(`${version}: `, '');
       }
 
+      packageJson.resolutions ??= {};
       packageJson.resolutions[reactPackageName] = packageVersion;
     }),
   );
