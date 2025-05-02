923a8e76523b0111bb32975453a417f0f00454fb
Olivier Tassinari <olivier.tassinari@gmail.com>
2025-05-02 02:38:44 +0200

    [code-infra] Give a stable name to dev tool app

--- dangerfile.ts
+++ dangerfile.ts
@@ -154,7 +154,7 @@ async function reportBundleSize() {
   const detailedComparisonQuery = `circleCIBuildNumber=${circleCIBuildNumber}&baseRef=${danger.github.pr.base.ref}&baseCommit=${comparison.previous}&prNumber=${danger.github.pr.number}`;
   const detailedComparisonToolpadUrl = `https://tools-public.mui.com/prod/pages/h71gdad?${detailedComparisonQuery}`;
   const detailedComparisonRoute = `/size-comparison?${detailedComparisonQuery}`;
-  const detailedComparisonUrl = `https://mui-dashboard.netlify.app${detailedComparisonRoute}`;
+  const detailedComparisonUrl = `https://frontend-public.mui.com${detailedComparisonRoute}`;
 
   const { all: allResults, main: mainResults } = sieveResults(Object.entries(comparison.bundles));
   const anyResultsChanges = allResults.filter(createComparisonFilter(1, 1));
