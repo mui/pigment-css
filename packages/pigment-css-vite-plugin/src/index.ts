382ce9b76a40780b035cfc5427902596963c27e8
Joseph Gao <Joseph Gao>
2025-06-06 14:14:31 +0800

    fix: seperator issue for Windows

--- packages/pigment-css-vite-plugin/src/index.ts
+++ packages/pigment-css-vite-plugin/src/index.ts
@@ -66,10 +66,10 @@ export function pigment(options: PigmentOptions) {
       name: 'pigment-css-theme-injection-plugin',
       enforce: 'pre',
       resolveId(source) {
-        if (finalTransformLibraries.some((lib) => source.includes(`${lib}/styles.css`))) {
+        if (finalTransformLibraries.some((lib) => source.includes(`${lib.replace(/\\/g, '/')}/styles.css`))) {
           return VIRTUAL_CSS_FILE;
         }
-        if (finalTransformLibraries.some((lib) => source.includes(`${lib}/theme`))) {
+        if (finalTransformLibraries.some((lib) => source.includes(`${lib.replace(/\\/g, '/')}/theme`))) {
           return VIRTUAL_THEME_FILE;
         }
         return null;
